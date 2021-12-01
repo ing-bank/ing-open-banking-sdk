d802e9c2ef89ca721ec7d611db067c1422d5a2cb
WK90JC <bernhard.heesen@ing.com>
2021-12-01 09:01:02 +0100

    feat: add simple app and update README

---
+++ java/open-banking-simple-app/src/main/java/com/ing/developer/app/ProductionShowcaseAPI.java
@@ -0,0 +1,27 @@
+package com.ing.developer.app;
+
+import com.ing.developer.common.Utils;
+import com.ing.developer.common.clients.Companion;
+import com.ing.developer.showcase.client.ApiClient;
+import com.ing.developer.showcase.client.api.GreetingsApi;
+
+import javax.ws.rs.client.ClientBuilder;
+import java.security.PrivateKey;
+
+public class ProductionShowcaseAPI {
+    public static void callShowcaseAPI() throws Exception {
+        // Set the BASE_URL=https://api.ing.com.
+        // Use own client key.
+        // Use certs uploaded to the developer portal
+        // Use keystore password.
+        String clientId = "3e19a706-511d-480c-ac3e-4ba135b154f4";
+        String keyStoreFileName = "keystore-premium.jks";
+        char[] keyStorePassword = "secret".toCharArray();
+
+        Utils.Pair<PrivateKey, ClientBuilder> trustMaterial = Companion.Utils.createOpenBankingClient(keyStoreFileName, keyStorePassword, false, null, null, false, false);
+        ApiClient clientAPI = new ApiClient(clientId, trustMaterial.getFirst(), trustMaterial.getSecond());
+        GreetingsApi greetingsApi = new GreetingsApi(clientAPI);
+        String greeting = greetingsApi.greetingsSingleGet(null).getMessage();
+        System.out.println(greeting);
+    }
+}
--- keygen-premium.sh
+++ keygen-premium.sh
@@ -5,5 +5,7 @@ TLS=example_client_tls
 SIGN=example_client_signing
 DESTINATION=java/open-banking-demo-app/src/main/resources/
 FILENAME=keystore-premium.jks
+SIMPLE_DESTINATION=java/open-banking-simple-app/src/main/resources/
 
 ./keygen-generic.sh $SRC_DIR $TLS $SIGN $DESTINATION $FILENAME
+cp "$DESTINATION$FILENAME" "$SIMPLE_DESTINATION$FILENAME"

d58789c97f8901628d6eed00b398405b27fdfdbd
Bernhard Heesen <benneb2@gmail.com>
2022-02-10 07:42:07 +0100

    Use single swagger spec (#19)
    * feat: use single swagger spec
    
    Co-authored-by: WK90JC <bernhard.heesen@ing.com>

--- scripts/getLatestSwaggerVersion.sh
+++ scripts/getLatestSwaggerVersion.sh
@@ -4,6 +4,10 @@ echo "apiId: $1"
 echo "swaggerDir:$2"
 prefix=")]}',"
 
+_jq() {
+    echo "${row}" | base64 --decode | jq -r "${1}"
+}
+
 raw_versions=$(curl "https://api.developer.ing.com/apis/$1/versions")
 
 cleaned_versions=$(echo "$raw_versions" | sed -e "s/^$prefix//") 
@@ -11,10 +15,6 @@ cleaned_versions=$(echo "$raw_versions" | sed -e "s/^$prefix//")
 echo "$cleaned_versions"
 
 for row in $(echo "${cleaned_versions}" | jq -r '.apis[] | @base64'); do
-    _jq() {
-     echo "${row}" | base64 --decode | jq -r "${1}"
-    }
-    
     name=$(_jq '.api.name')
     versionNumber=$(_jq '.versionNumber')
     versionId=$(_jq '.versionId')
@@ -27,13 +27,22 @@ for row in $(echo "${cleaned_versions}" | jq -r '.apis[] | @base64'); do
     echo "$status"
 
     if [ "$status" == "LIVE" ]; then 
-	file_name=$(echo "api/${2}/${name}-${versionNumber}.json" | tr " " "-")
+	file_name=$(echo "api/${2}/${name}.json" | tr " " "-")
         if [ ! -f "$file_name" ]; then
              echo "$file_name does not exist. Downloading...."
              curl "https://api.developer.ing.com/apis/$1/versions/${versionId}/specification/download?format=json&pretty=true&resolved=false" -o "$file_name"
 	     git add "$file_name"
         else
              echo "$file_name exist."
+             currentVersion=$(jq -r .info.version "${file_name}")
+             echo "currentversion: $currentVersion"
+             if [ $versionNumber == $currentVersion ]; then
+                 echo "VERSION MATCH"        
+             else
+                 echo "DOWNLOAD NEW VERSION"
+                 curl "https://api.developer.ing.com/apis/$1/versions/${versionId}/specification/download?format=json&pretty=true&resolved=false" -o "$file_name"
+                 git add "$file_name"
+             fi
         fi
     fi
     echo "--------------"

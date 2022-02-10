07541e5b0d69785f17ad52c8b19e2528313fbe55
WK90JC <bernhard.heesen@ing.com>
2022-02-10 07:34:15 +0100

    feat: use single swagger spec

--- scripts/getLatestSwaggerVersion.sh
+++ scripts/getLatestSwaggerVersion.sh
@@ -4,16 +4,19 @@ echo "apiId: $1"
 echo "swaggerDir:$2"
 prefix=")]}',"
 
+_jq() {
+    echo "${row}" | base64 --decode | jq -r "${1}"
+}
+
 raw_versions=$(curl "https://api.developer.ing.com/apis/$1/versions")
 
 cleaned_versions=$(echo "$raw_versions" | sed -e "s/^$prefix//") 
 
 echo "$cleaned_versions"
 
+
+
 for row in $(echo "${cleaned_versions}" | jq -r '.apis[] | @base64'); do
-    _jq() {
-     echo "${row}" | base64 --decode | jq -r "${1}"
-    }
     
     name=$(_jq '.api.name')
     versionNumber=$(_jq '.versionNumber')
@@ -27,13 +30,23 @@ for row in $(echo "${cleaned_versions}" | jq -r '.apis[] | @base64'); do
     echo "$status"
 
     if [ "$status" == "LIVE" ]; then 
-	file_name=$(echo "api/${2}/${name}-${versionNumber}.json" | tr " " "-")
+	file_name=$(echo "api/${2}/${name}.json" | tr " " "-")
+
         if [ ! -f "$file_name" ]; then
              echo "$file_name does not exist. Downloading...."
              curl "https://api.developer.ing.com/apis/$1/versions/${versionId}/specification/download?format=json&pretty=true&resolved=false" -o "$file_name"
 	     git add "$file_name"
         else
              echo "$file_name exist."
+             currentVersion=$(jq -r .info.version "${file_name}")
+             echo "currentversion: $currentVersion"
+             if [ $versionNumber == $currentVersion ]; then
+                 echo "VERSION MATCH"        
+             else
+                 echo "DOWNLOAD NEW VERSION"
+                 curl "https://api.developer.ing.com/apis/$1/versions/${versionId}/specification/download?format=json&pretty=true&resolved=false" -o "$file_name"
+                 git add "$file_name"
+             fi
         fi
     fi
     echo "--------------"

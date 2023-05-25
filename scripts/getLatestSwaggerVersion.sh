23a49e041434c1ffc8eb67ddf39d65ed27e3d800
Alexandru Ionut Balan <alexandru.ionut.balan@proton.me>
2023-05-25 11:23:23 +0300

    fix: Scripts now follow correct path
    At some points the scripts were moved around but the paths inside were not updated.
    
    (cherry picked from commit dd1680eb5ef78c9c7bf8fac2b81ff2bf9cdf3383)

--- scripts/getLatestSwaggerVersion.sh
+++ scripts/getLatestSwaggerVersion.sh
@@ -29,7 +29,7 @@ for row in $(echo "${cleaned_versions}" | jq -r '.apis[] | @base64'); do
     echo "$status"
 
     if [ "$status" == "LIVE" ]; then
-	    file_name=$(echo "api/${SWAGGER_DIR}/${name}.json" | tr " " "-")
+	    file_name=$(echo "../api/${SWAGGER_DIR}/${name}.json" | tr " " "-")
         if [ ! -f "$file_name" ]; then
              echo "$file_name does not exist. Downloading...."
              curl "https://api.developer.ing.com/apis/$API_ID/versions/${versionId}/specification/download?format=json&pretty=true&resolved=false" -o "$file_name"
